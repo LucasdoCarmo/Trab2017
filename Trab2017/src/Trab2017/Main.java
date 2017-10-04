0d60435d0f533b75313ac9d6485b82607f65fe94
Lucas <Lucas@ASUS>
2017-10-04 15:16:22 -0300

    3

---
+++ Trab2017/src/Trab2017/Main.java
@@ -0,0 +1,39 @@
+package Trab2017;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lombok.Getter;
+
+public class Main extends Application {
+
+	public static final String PATH_VIEW = "../telas/";
+	
+	@Getter
+	private static AnchorPane rootLayout;
+	@Getter
+	private static Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Main.primaryStage = primaryStage;
+			Main.rootLayout = (AnchorPane) FXMLLoader.load(getClass().getResource(PATH_VIEW + "Login.fxml"));
+
+			primaryStage.setTitle("Restaurantes Famosos");
+			Scene scene = new Scene(rootLayout);
+			scene.getStylesheets().add("/css/style.css");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
